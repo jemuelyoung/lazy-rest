66663a793ee1f51ae2932cab7cf02e2982a5e5f6
Jem Young <jemuel.young@gmail.com>
2013-03-07 23:18:49 -0500

    First version

--- lazy-rest.py
+++ lazy-rest.py
@@ -2,3 +2,13 @@
 Author: Jem Young - jemuel.young@gmail.com
 Date: 3/2013
 """
+
+import urllib
+import urllib2
+
+url = raw_input("URL: ").strip()
+request = urllib2.Request(url)
+request.add_header = ('Accept', 'application/json')
+response = urllib2.urlopen(request)
+response.read()
+
