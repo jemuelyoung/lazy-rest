cffb565aca600bed43bad28388fc5833b419e4e6
Jem Young <jemuel.young@gmail.com>
2013-03-10 23:51:32 -0400

    add javascript

---
+++ lazy-rest.js
@@ -0,0 +1,17 @@
+;
+function input_processing(url, data){
+	var self = this, request;	
+	request = new XMLHttpRequest();
+	
+	if(url.lastindexof("http://",0) === 0){
+		url = "http://" + url;
+	}
+	
+	if(data === null){
+		request.open("GET", url, true);
+	} else{
+		request.open("POST", url, true);
+		request.setRequestHeader("Content-type","multipart/form-data");
+	}
+	
+}
